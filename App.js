20197af144575aca40aebd6931f1bd9dfdb9f65e
polatemre <41800395+polatemre@users.noreply.github.com>
2020-12-27 12:16:19 +0300

    init firebase and to do

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import * as firebase from 'firebase';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Selamun aleykum</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Main from './app/components/main';
+
+export default class App extends React.Component {
+
+    componentDidMount = () => {
+      var firebaseConfig = {
+        apiKey: "AIzaSyABMGZ365YGID71HiweejF7YbCXWo0PrQg",
+        authDomain: "todoapp-d0285.firebaseapp.com",
+        projectId: "todoapp-d0285",
+        storageBucket: "todoapp-d0285.appspot.com",
+        messagingSenderId: "837452800896",
+        appId: "1:837452800896:web:862fc414d58487295e1765",
+        measurementId: "G-JZ42DBFYWW"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+    }
+
+    render() {
+        return (<Main/>);
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
